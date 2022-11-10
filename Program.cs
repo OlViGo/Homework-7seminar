f8ddb3c7a0a8e42f0466ebbf1e9fb1f9929d330b
OlViGo <olgabibendum@gmail.com>
2022-11-11 02:26:05 +0300

    Homework7

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿// Задача 50. Напишите программу, которая на вход принимает  позиции элемента
+//  в двумерном массиве, и возвращает значение этого элемента или же указание,
+//  что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// [1,7] -> такого числа в массиве нет ([1,7] это позиция элемента)
+
+Console.WriteLine("введите индекс строки");
+int rows = int.Parse(Console.ReadLine());
+Console.WriteLine("введите индекс столбца");
+int columns = int.Parse(Console.ReadLine());
+int[,] numbers = new int[10, 10];
+FillArray(numbers);
+
+
+if (rows < numbers.GetLength(0) && columns < numbers.GetLength(1))
+{
+Console.WriteLine($"значение элемента {rows} и {columns} равно {numbers [rows-1,columns-1]}");
+}
+else
+{
+    Console.WriteLine($"{rows}{columns} -> такого элемента в массиве нет");
+}
+PrintArray(numbers);
+void FillArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(-100, 100)/10;
+        }
+    }
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
---
+++ Task1/Program.cs
@@ -0,0 +1,56 @@
+﻿// Задайте двумерный массив из целых чисел. Найдите среднее 
+// арифметическое элементов в каждом столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+void Main()
+{
+    Console.Write("Введите количество строк: ");
+    int rows = int.Parse(Console.ReadLine());
+    Console.Write("Введите количество столбцов: ");
+    int columns = int.Parse(Console.ReadLine());
+    int[,] arithmetic = new int[rows, columns];
+    NewArray(arithmetic);
+    PrintArray(arithmetic);
+
+    for (int j = 0; j < arithmetic.GetLength(1); j++)
+    {
+        double avarage = 0;
+        for (int i = 0; i < arithmetic.GetLength(0); i++)
+        {
+            avarage = (avarage + arithmetic[i, j]);
+        }
+        avarage = avarage / rows;
+        Console.Write(avarage + "; ");
+    }
+    Console.WriteLine();
+}
+void NewArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            {
+                array[i, j] = new Random().Next(0, 10);
+            }
+        }
+
+    }
+}
+
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+Main();
